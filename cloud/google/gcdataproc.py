13ddf8e4d09b78b803d70cfd845ac15e341a1fc8
John Baublitz <john@cambridgesemantics.com>
2016-10-20 22:29:05 +0000

    Finished documentation prototype

--- cloud/google/gcdataproc.py
+++ cloud/google/gcdataproc.py
@@ -110,9 +110,44 @@ notes:
 '''
 
 EXAMPLES = '''
+# Creating a dataproc cluster with default paramters
+- gcdataproc:
+    name: test
+
+# Creating a dataproc cluster with defaults, fire and forget
+- gcdataproc:
+    name: test
+    sync: false
 '''
 
 RETURN = '''
+name:
+  description:
+    - The server-assigned name, which is only unique within the same service that originally returns it. If
+      you use the default HTTP mapping, the name should have the format of operations/some/unique/name.
+metadata:
+  description:
+    - Service-specific metadata associated with the operation. It typically contains progress information
+      and common metadata such as create time. Some services might not provide such metadata. Any method
+      that returns a long-running operation should document the metadata type, if any.
+      An object containing fields of an arbitrary type. An additional field "@type" contains a URI
+      identifying the type. Example: { "id": 1234, "@type": "types.example.com/standard/id" }.
+done:
+  description:
+    - If the value is false, it means the operation is still in progress. If true, the operation is completed,
+      and either error or response is available.
+error:
+  description:
+    - The error result of the operation in case of failure. 
+response:
+  description:
+    - The normal response of the operation in case of success. If the original method returns no data on
+      success, such as Delete, the response is google.protobuf.Empty. If the original method is standard
+      Get/Create/Update, the response should be the resource. For other methods, the response should have
+      the type XxxResponse, where Xxx is the original method name. For example, if the original method
+      name is TakeSnapshot(), the inferred response type is TakeSnapshotResponse.
+      An object containing fields of an arbitrary type. An additional field "@type" contains a URI
+      identifying the type. Example: { "id": 1234, "@type": "types.example.com/standard/id" }.
 '''
 
 import os
